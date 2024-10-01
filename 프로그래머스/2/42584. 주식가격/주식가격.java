163d9fdb9d4902e868b4d053d23307b0cf710efb
Seohyun Jung <tjgus52213@gmail.com>
2024-10-01 18:06:40 +0900

    [level 2] Title: 주식가격, Time: 15.68 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for (int i = 0; i < prices.length - 1; i++) {
+            int count = 0;
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] <= prices[j]) {
+                    count++;
+                } else {
+                    count++;
+                    break;   
+                }
+            }
+            answer[i] = count;
+        }
+        answer[prices.length - 1] = 0;
+        
+        return answer;
+    }
+}
