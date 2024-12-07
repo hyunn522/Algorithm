729096a0821ed7bf6eda278bb7e0e5b69d0a81e4
Seohyun Jung <tjgus52213@gmail.com>
2024-12-07 17:45:31 +0900

    [Silver III] Title: N과 M (4), Time: 176 ms, Memory: 19652 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        br.close();
+
+        arr = new int[m];
+
+        rec(1, 0);
+        System.out.println(sb);
+    }
+
+    private static void rec(int start, int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            rec(i, depth + 1);
+        }
+    }
+
+}
