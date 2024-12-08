50fe082b649923f772ada5988b82bc63d8d86d1e
Seohyun Jung <tjgus52213@gmail.com>
2024-12-08 15:30:04 +0900

    [Silver III] Title: N과 M (6), Time: 116 ms, Memory: 15904 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,7 +7,6 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static int n, m;
     static int[] arrN, arrM;
-    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +16,6 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         arrN = new int[n];
         arrM = new int[m];
-        visited = new boolean[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
@@ -42,12 +38,8 @@ public class Main {
         }
 
         for (int i = start; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arrM[depth] = arrN[i];
-                rec(i + 1, depth + 1);
-                visited[i] = false;
-            }
+            arrM[depth] = arrN[i];
+            rec(i + 1, depth + 1);
         }
     }
 
