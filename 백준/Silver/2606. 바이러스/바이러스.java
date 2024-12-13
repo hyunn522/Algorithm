69a99b4e9d491f981a4590f0ea34f77135255747
Seohyun Jung <tjgus52213@gmail.com>
2024-12-13 19:22:23 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,12 +1,10 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     private static int n;
-    private static List<List<Integer>> graph = new ArrayList<>();
+    private static int[][] graph;
     private static boolean[] visited;
     private static int result = 0;
 
@@ -14,9 +12,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
+        graph = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
 
         int m = Integer.parseInt(br.readLine());
@@ -24,10 +20,14 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int c1 = Integer.parseInt(st.nextToken());
             int c2 = Integer.parseInt(st.nextToken());
-            graph.get(c1).add(c2);
-            graph.get(c2).add(c1);
+            graph[c1][c2] = 1;
+            graph[c2][c1] = 1;
         }
         br.close();
+
+        for (int i = 1; i <= n; i++) {
+            graph[i][i] = 1;
+        }
 
         dfs(1);
 
@@ -37,9 +37,9 @@ public class Main {
     private static void dfs(int node) {
         visited[node] = true;
 
-        for (int next : graph.get(node)) {
-            if (!visited[next]) {
-                dfs(next);
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && graph[node][i] == 1) {
+                dfs(i);
                 result++;
             }
         }
