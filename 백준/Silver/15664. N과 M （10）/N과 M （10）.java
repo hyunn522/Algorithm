8fb660fb5a1c6f1e6c07548525541298b37c2760
Seohyun Jung <tjgus52213@gmail.com>
2024-12-20 17:18:07 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n, m;
+    private static List<Integer> arr;
+    private static boolean[] visited;
+    private static int[] result;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new ArrayList<>();
+        visited = new boolean[n];
+        result = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+        br.close();
+
+        Collections.sort(arr);
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int start, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = -1;
+        for (int i = start; i < n; i++) {
+            if (!visited[i] && before != arr.get(i)) {
+                before = arr.get(i);
+                visited[i] = true;
+                result[depth] = arr.get(i);
+                dfs(i + 1, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
