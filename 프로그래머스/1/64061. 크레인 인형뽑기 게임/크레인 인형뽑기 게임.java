fa522b40160d4ef2b4be87a1f5a6bc647889c247
Seohyun Jung <tjgus52213@gmail.com>
2025-02-28 17:42:21 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.84 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -6,11 +6,8 @@ class Solution {
         int answer = 0;
         
         Deque<Integer> stack = new LinkedList<>();
-        boolean[] isEmpty = checkIsEmpty(board);
         
         for (int move : moves) {
-            if (isEmpty[move - 1]) continue;
-            
             for (int i = 0; i < n; i++) {
                 int doll = board[i][move - 1];
                 if (doll != 0) {
@@ -26,26 +23,10 @@ class Solution {
                             stack.push(doll);
                         }
                     }
-
-                    isEmpty = checkIsEmpty(board);
                     break;
                 }
             }
         }
         return answer;
-    }
-    
-    private boolean[] checkIsEmpty(int[][] board) {
-        boolean[] isEmpty = new boolean[board.length];
-        for (int i = 0; i < board.length; i++) {
-            isEmpty[i] = true;
-            for (int j = 0; j < board.length; j++) {
-                if (board[j][i] > 0) {
-                    isEmpty[i] = false;
-                    continue;
-                }
-            }
-        }
-        return isEmpty;
     }
 }
