107008d9ffae49b8cdf35c41df8c7ff51b24b633
Seohyun Jung <tjgus52213@gmail.com>
2025-03-31 13:59:29 +0900

    [Silver I] Title: 현수막, Time: 228 ms, Memory: 26320 KB -BaekjoonHub

---
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int m, n;
+    static int[][] paper;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
+    static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        paper = new int[m][n];
+        visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (paper[i][j] == 1 && !visited[i][j]) {
+                    dfs(i, j);
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if (paper[nx][ny] == 0 || visited[nx][ny]) continue;
+            dfs(nx, ny);
+        }
+    }
+
+}
