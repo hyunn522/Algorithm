d4a3e5340e043a37e600281e4e71938f16a8e57a
Seohyun Jung <tjgus52213@gmail.com>
2025-04-09 11:09:52 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 124 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
