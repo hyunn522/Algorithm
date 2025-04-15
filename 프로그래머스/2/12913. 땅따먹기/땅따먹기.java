a9373a188d283fbf440f468ee6d4da945ac451e8
Seohyun Jung <tjgus52213@gmail.com>
2025-04-15 10:48:19 +0900

    [level 2] Title: 땅따먹기, Time: 28.68 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,16 @@
+class Solution {
+    int solution(int[][] land) {
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
+        
+        int answer = 0;
+        for (int i : land[land.length - 1]) {
+            if (i > answer) answer = i;
+        }
+        return answer;
+    }
+}
