0026c442b13bc37e2e0e09986cf489d7e27dde85
Seohyun Jung <tjgus52213@gmail.com>
2025-05-22 21:44:43 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 104 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -10,16 +10,13 @@ class Solution {
 
         for (int test_case = 1; test_case <= T; test_case++) {
             char[] original = sc.next().toCharArray();
-
             char cur = '0';
             int answer = 0;
+
             for (int i = 0; i < original.length; i++) {
-                if (original[i] == '1' && cur == '0') {
+                if (original[i] != cur) {
                     answer++;
-                    cur = '1';
-                } else if (original[i] == '0' && cur == '1') {
-                    answer++;
-                    cur = '0';
+                    cur = original[i];
                 }
             }
 
