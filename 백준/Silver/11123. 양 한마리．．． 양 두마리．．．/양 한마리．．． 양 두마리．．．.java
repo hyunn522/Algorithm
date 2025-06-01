9ee50c704667df72e49750d30f9e1ac9aca9014e
Seohyun Jung <tjgus52213@gmail.com>
2025-06-01 11:07:36 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 240 ms, Memory: 26368 KB -BaekjoonHub

---
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int h, w;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            map = new char[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                String input = br.readLine();
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = input.charAt(j);
+                }
+            }
+
+            int answer = 0;
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && map[i][j] == '#') {
+                        bfs(i, j);
+                        answer++;
+                    }
+                }
+            }
+            sb.append(answer + "\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curX = cur[0];
+            int curY = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+
+                if (!visited[nx][ny] && map[nx][ny] == '#') {
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+}
