ef1efd1fb4e9b403e8b81e7aaba30c53d0d98c7c
Seohyun Jung <tjgus52213@gmail.com>
2025-06-07 21:20:37 +0900

    [Silver V] Title: Best Grass, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/6186. Best Grass/Best Grass.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int r, c, answer = 0;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        map = new char[r][c];
+        visited = new boolean[r][c];
+
+        for (int i = 0; i < r; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < c; j++) {
+                map[i][j] = input.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (!visited[i][j] && map[i][j] == '#') bfs(i, j);
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
+
+                if (!visited[nx][ny] && map[nx][ny] == '#') {
+                    visited[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny});
+                }
+            }
+        }
+
+        answer++;
+    }
+}
