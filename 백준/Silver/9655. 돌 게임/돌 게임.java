ad2750a74ac1541f8e11ab9775f43d0b31ade0d6
Seohyun Jung <tjgus52213@gmail.com>
2025-06-14 21:44:21 +0900

    [Silver V] Title: 돌 게임, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 == 0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+    }
+}
