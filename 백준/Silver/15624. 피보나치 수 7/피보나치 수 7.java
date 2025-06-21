7deb071ffe3982862c518a8c9f64a895c7d0d025
Seohyun Jung <tjgus52213@gmail.com>
2025-06-21 22:16:04 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 120 ms, Memory: 21928 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        long[] dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
