082b11da66567acaf36ccec7bcaec20d02b4880c
Seohyun Jung <tjgus52213@gmail.com>
2025-08-18 22:43:56 +0900

    [Gold V] Title: 집합의 표현, Time: 524 ms, Memory: 54484 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (command == 0) {
+                union(a, b);
+            } else {
+                sb.append(find(a) == find(b) ? "YES\n" : "NO\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static void union(int x, int y) {
+        int parentX = find(x);
+        int parentY = find(y);
+
+        if (parentX != parentY) {
+            if (parentX < y) {
+                parent[parentY] = parentX;
+            } else {
+                parent[parentX] = parentY;
+            }
+        }
+    }
+
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        } else {
+            return parent[x] = find(parent[x]);
+        }
+    }
+}
