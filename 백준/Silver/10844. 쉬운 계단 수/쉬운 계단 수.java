06706d4ee9039dbbcb29534910ba50eb999be860
Seohyun Jung <tjgus52213@gmail.com>
2025-08-21 18:32:54 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[n + 1][10]; // 길이가 i고 마지막 수가 j인 계단 수의 개수
+        dp[1][0] = 0;
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j + 1];
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+                }
+            }
+        }
+
+        long answer = 0;
+        for (long i : dp[n]) {
+            answer += i;
+        }
+        System.out.println(answer % 1000000000);
+    }
+}
