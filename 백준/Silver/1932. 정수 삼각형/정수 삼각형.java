efc8a16c5d37eb632a32128a2806389e69b2b87c
Seohyun Jung <tjgus52213@gmail.com>
2025-08-24 21:51:25 +0900

    [Silver I] Title: 정수 삼각형, Time: 248 ms, Memory: 26744 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -3,31 +3,34 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int[][] graph = new int[n][n]; // 삼각형
-        int[][] dp = new int[n][n]; // 각 층의 각 칸에서의 최대 합
+        int[][] triangle = new int[n][n];
 
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < i + 1; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+                triangle[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dp[0][0] = graph[0][0];
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle[0][0];
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j > 0) dp[i][j] = (Math.max(dp[i - 1][j - 1], dp[i - 1][j])) + graph[i][j];
-                else dp[i][j] = dp[i - 1][j] + graph[i][j];
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+            
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
             }
         }
 
-        int max = 0;
+        int answer = Integer.MIN_VALUE;
         for (int i : dp[n - 1]) {
-            max = Math.max(max, i);
+            answer = Math.max(answer, i);
         }
-        System.out.println(max);
+        System.out.println(answer);
     }
 }
