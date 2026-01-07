77152d6a6654454b87e1e65486c9a2657ed505b2
Seohyun Jung <tjgus52213@gmail.com>
2026-01-07 11:54:46 +0900

    [Silver I] Title: 비밀번호, Time: 124 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/2780. 비밀번호/비밀번호.java
+++ 백준/Silver/2780. 비밀번호/비밀번호.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int tc = Integer.parseInt(br.readLine());
-        int[][] nears = {
+
+        int[][] adj = {
                 {7},
                 {2, 4},
                 {1, 3, 5},
@@ -19,30 +19,24 @@ public class Main {
                 {5, 7, 9},
                 {6, 8}
         };
-        int NUM_CNT = 10;
-        int MOD = 1234567;
 
-        int[][] dp = new int[1001][NUM_CNT]; // 길이가 i면서 j로 끝나는 경우의 수
+        int[][] dp = new int[1001][10]; // 길이가 i면서 j로 끝나는 비밀번호의 개수
+        Arrays.fill(dp[1], 1);
 
-        for (int i = 1; i <= 1000; i++) {
-            if (i == 1) {
-                Arrays.fill(dp[1], 1);
-                continue;
-            }
-
-            for (int j = 0; j < NUM_CNT; j++) {
-                for (int near : nears[j]) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][near]) % MOD;
+        for (int i = 2; i <= 1000; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k : adj[j]) {
+                    dp[i][j] = (dp[i - 1][k] + dp[i][j]) % 1234567;
                 }
             }
         }
 
+        int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
             int n = Integer.parseInt(br.readLine());
-
             int answer = 0;
-            for (int i = 0; i < NUM_CNT; i++) {
-                answer = (answer + dp[n][i]) % MOD;
+            for (int i = 0; i < 10; i++) {
+                answer = (answer + dp[n][i]) % 1234567;
             }
             sb.append(answer + "\n");
         }
