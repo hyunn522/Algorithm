b1c4890c104991521c716655ef3664bee4869992
Seohyun Jung <tjgus52213@gmail.com>
2026-01-09 17:31:09 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18032 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,19 +2,19 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1]; // i를 1로 만들 때까지의 최소 연산 횟수
-        dp[0] = dp[1] = 0;
+        int[] dp = new int[n + 1]; // i를 1로 만들기 위한 최소 연산 횟수
 
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1; // 1을 뺀 값
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 1을 빼는 경우와 2로 나누는 경우 중 연산 횟수가 더 작은 경우 선택
+            dp[i] = dp[i - 1] + 1; // 1을 빼는 경우
+
+            if (i % 2 == 0) { // 2로 나누어떨어지는 경우
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-            if (i % 3 == 0) { 
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 1을 빼는 경우와 3으로 나누는 경우 중 연산 횟수가 더 작은 경우 선택
+            if (i % 3 == 0) { // 3으로 나누어떨어지는 경우
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
 
