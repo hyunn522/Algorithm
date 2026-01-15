5ee7fdf5f97e8721d26772c26d6810093f5a1b5b
Seohyun Jung <tjgus52213@gmail.com>
2026-01-15 14:18:25 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 116 ms, Memory: 21868 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
@@ -1,42 +1,29 @@
 import java.io.*;
 
 public class Main {
-
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int absN = Math.abs(n);
 
-        if (n == 0) {
-            System.out.println(0);
-            System.out.println(0);
-        } else if (n > 0) {
-            if (n == 1) {
-                System.out.println(1);
-                System.out.println(1);
-                return;
-            }
-
-            long[] dp = new long[n + 1];
-            dp[0] = 0;
-            dp[1] = 1;
-            for (int i = 2; i <= n; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000000;
-            }
-
-            System.out.println(dp[n] < 0 ? -1 : 1);
-            System.out.println(dp[n]);
-        } else {
-            n *= -1; // 양수로 변환
-
-            long[] dp = new long[n + 2];
-            dp[n + 1] = 1;
-            dp[n] = 0;
-            for (int i = n - 1; i >= 0; i--) {
-                dp[i] = (dp[i + 2] - dp[i + 1]) % 1000000000;
-            }
-
-            System.out.println(dp[0] < 0 ? -1 : 1);
-            System.out.println(dp[0] < 0 ? dp[0] * -1 : dp[0]);
+        if (absN == 0) {
+            System.out.println(0 + "\n" + 0);
+            return;
         }
+
+        if (absN == 1 || absN == 2) {
+            System.out.println(n == -2 ? -1 : 1);
+            System.out.println(1);
+            return;
+        }
+
+        long[] dp = new long[absN + 1];
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= absN; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000000;
+        }
+
+        System.out.println(n > 0 ? 1 : (n == 0 ? 0 : (absN % 2 == 1 ? 1 : -1)));
+        System.out.println(dp[absN]);
     }
 }
