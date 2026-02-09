732f7721ccbc0ceb1d798f6ef436918a15598159
Seohyun Jung <tjgus52213@gmail.com>
2026-02-09 17:16:58 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 140 ms, Memory: 16824 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -1,58 +1,54 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main { 
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int[][] dp = new int[n][2]; // {현재까지의 최대 길이, 바로 이전의 인덱스}
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for (int i = 0; i < n; i++) {
-			dp[i][0] = 1;
-			dp[i][1] = i;
-			
-			for (int j = 0; j < i; j++) {
-				if (arr[j] < arr[i]) {
-					if (arr[j] < arr[i] && dp[i][0] < dp[j][0] + 1) { // 최대 수열 갱신
-						dp[i][0] = dp[j][0] + 1;
-						dp[i][1] = j;
-					}
-				}
-			}
-		}
-		
-		int maxIdx = 0;
-		for (int i = 0; i < n; i++) {
-			if (dp[i][0] > dp[maxIdx][0]) {
-				maxIdx = i;
-			}
-		}
+public class Main {
 
-		int idx = maxIdx;
-		List<Integer> answer = new ArrayList<>();
-		while (true) {
-			answer.add(arr[idx]);
-			if (dp[idx][1] == idx) break;
-			idx = dp[idx][1];
-		}
-		Collections.reverse(answer);
-		
-		sb.append(answer.size() + "\n");
-		for (int i : answer) {
-			sb.append(i + " ");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n]; // i번째 수까지의 가장 긴 증가하는 부분 수열의 길이
+        int[] idxes = new int[n]; // 가장 긴 증가하는 부분 수열에서 i번째 수 이전 수의 인덱스
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        idxes[i] = j;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int maxIdx = -1;
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > maxLength) {
+                maxLength = dp[i];
+                maxIdx = i;
+            }
+        }
+        sb.append(maxLength + "\n");
+
+        int[] maxArr = new int[maxLength];
+        for (int i = maxLength - 1; i >= 0; i--) {
+            maxArr[i] = arr[maxIdx];
+            maxIdx = idxes[maxIdx];
+        }
+
+        for (int i : maxArr) {
+            sb.append(i + " ");
+        }
+        System.out.println(sb);
+    }
 }
