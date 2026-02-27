1f64d8e56f35f85a881ed209e3a3adab845e5db0
Seohyun Jung <tjgus52213@gmail.com>
2026-02-27 17:42:28 +0900

    [Silver I] Title: 절댓값 힙, Time: 296 ms, Memory: 26420 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,30 +1,38 @@
 import java.io.*;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> {
-            int abs1 = Math.abs(n1);
-            int abs2 = Math.abs(n2);
+        // 절댓값 기준으로 정렬
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x1, x2) -> {
+            int absX1 = Math.abs(x1);
+            int absX2 = Math.abs(x2);
 
-            if (abs1 == abs2) {
-                return n1 - n2;
+            if (absX1 != absX2) {
+                return absX1 - absX2;
             } else {
-                return abs1 - abs2;
+                return x1 - x2;
             }
         });
 
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());
-            if (x == 0) {
-                System.out.println(pq.isEmpty() ? 0 : pq.poll());
+            if (x != 0) {
+                pq.add(x);
             } else {
-                pq.offer(x);
+                if (pq.isEmpty()) {
+                    sb.append(0 + "\n");
+                } else {
+                    sb.append(pq.poll() + "\n");
+                }
             }
         }
+
+        System.out.println(sb);
     }
 }
