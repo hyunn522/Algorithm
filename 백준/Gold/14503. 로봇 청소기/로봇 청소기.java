a92b6bf8fd1b6cbce279e434278a69e2abb702e0
Seohyun Jung <tjgus52213@gmail.com>
2026-02-28 11:03:32 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -3,19 +3,13 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    /*
-    0 = 아직 청소되지 않음, 1 = 벽, 2 = 청소됨
-     */
-    private static int n, m, d;
-    private static int[][] room;
-    // 북, 동, 남, 서
-    private static int[] dr = {-1, 0, 1, 0};
-    private static int[] dc = {0, 1, 0, -1};
-    private static int result = 0;
+    static int n, m, d, answer = 0;
+    static int[][] room; // 0 : 벽, 1 : 청소되지 않은 빈 칸, 2 : 청소된 빈 칸
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
@@ -35,34 +29,32 @@ public class Main {
 
         clean(r, c);
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 
-    private static void clean(int r, int c) {
-        // 현재 칸이 청소되지 않은 경우
-        if (room[r][c] == 0) {
-            room[r][c] = 2;
-            result++;
+    private static void clean(int x, int y) {
+        // 현재 칸이 청소되어 있지 않은 경우
+        if (room[x][y] == 0) {
+            room[x][y] = 2;
+            answer++;
         }
 
-        // 현재 칸의 동서남북 청소
+        // 주변 4칸 청소
         for (int i = 0; i < 4; i++) {
-            d = (d + 3) % 4; // 반시계 방향으로 회전
-            int nr = r + dr[d];
-            int nc = c + dc[d];
-
-            if (nr >= 0 && nc >= 0 && nr < n && nc < m && room[nr][nc] == 0) {
-                clean(nr, nc);
+            d = (d + 3) % 4; // 반시계 방향으로 90도 회전
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m && room[nx][ny] == 0) {
+                clean(nx, ny);
                 return;
             }
         }
 
-        // 현재 칸의 동서남북이 모두 청소된 경우
-        int backD = (d + 2) % 4;
-        int nr = r + dr[backD];
-        int nc = c + dc[backD];
-        if (nr >= 0 && nc >= 0 && nr < n && nc < m && room[nr][nc] != 1) {
-            clean(nr, nc);
+        // 주변 4칸이 이미 청소되어있는 경우 방향 유지하며 후진
+        int nx = x + dx[(d + 2) % 4];
+        int ny = y + dy[(d + 2) % 4];
+        if (nx >= 0 && ny >= 0 && nx < n && ny < m && room[nx][ny] != 1) {
+            clean(nx, ny);
         }
     }
 }
