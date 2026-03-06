01b9b49dfa300e36a95d5ec7b551458ae8edd582
Seohyun Jung <tjgus52213@gmail.com>
2026-03-07 01:37:08 +0900

    [Gold IV] Title: 최단경로, Time: 664 ms, Memory: 111844 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,81 +1,65 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static StringTokenizer st;
-    static List<List<Node>> graph;
-    static boolean[] visited;
-    static int[] cost;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
         int k = Integer.parseInt(br.readLine()) - 1;
 
-        graph = new ArrayList<>();
-        visited = new boolean[v];
-        cost = new int[v];
-
-        for (int i = 0; i < v; i++) {
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
             graph.add(new ArrayList<>());
         }
-        Arrays.fill(cost, Integer.MAX_VALUE);
 
-        for (int i = 0; i < e; i++) {
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
             int w = Integer.parseInt(st.nextToken());
-            graph.get(a).add(new Node(b, w));
+            graph.get(u).add(new int[]{v, w});
         }
 
-        dijkstra(k);
+        // 시작점에서 각 정점까지의 간선
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(p -> p[1]));
+        pq.offer(new int[]{k, 0});
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
 
-        for (int c : cost) {
-            System.out.println(c == Integer.MAX_VALUE ? "INF" : c);
-        }
-    }
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int curV = cur[0];
+            int curW = cur[1];
 
-    private static void dijkstra(int start) {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(start, 0));
-        cost[start] = 0;
+            if (dist[curV] < curW) {
+                continue;
+            }
 
-        while (!queue.isEmpty()) {
-            Node curNode = queue.poll();
-            int cur = curNode.point;
-
-            if (!visited[cur]) {
-                visited[cur] = true;
-
-                for (Node node : graph.get(cur)) {
-                    if (!visited[node.point] && cost[cur] + node.weight < cost[node.point]) {
-                        cost[node.point] = cost[cur] + node.weight;
-                        queue.add(new Node(node.point, cost[node.point]));
-                    }
+            // u와 연결된 간선 확인
+            for (int[] edge : graph.get(curV)) {
+                int nextV = edge[0];
+                int nextW = edge[1];
+                if (dist[curV] + nextW < dist[nextV]) {
+                    dist[nextV] = dist[curV] + nextW;
+                    pq.offer(new int[]{nextV, dist[nextV]});
                 }
             }
         }
-    }
 
-    static class Node implements Comparable<Node> {
-        int point, weight;
-
-        Node(int point, int weight) {
-            this.point = point;
-            this.weight = weight;
+        StringBuilder sb = new StringBuilder();
+        for (int i : dist) {
+            sb.append(i == Integer.MAX_VALUE ? "INF" : i).append("\n");
         }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.weight - o.weight;
-        }
+        System.out.println(sb);
     }
 }
