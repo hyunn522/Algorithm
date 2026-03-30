d2bae0b67dca70bce43aaefa7ee75db3a4ac6996
Seohyun Jung <tjgus52213@gmail.com>
2026-03-30 16:58:02 +0900

    [Silver II] Title: 병사 배치하기, Time: 140 ms, Memory: 14512 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 1;
+        int[] dp = new int[n]; // i번째 병사까지 봤을 때의 남아있는 병사 수
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1; // 자기 자신만 포함
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(n - answer);
+    }
+}
