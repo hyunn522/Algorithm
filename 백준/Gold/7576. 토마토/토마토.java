4f86e7f07b5dff633bb48039ad25896d3675e88a
Seohyun Jung <tjgus52213@gmail.com>
2026-04-03 02:26:43 +0900

    [Gold V] Title: 토마토, Time: 560 ms, Memory: 104192 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,94 +1,70 @@
 import java.io.*;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int m, n;
-    static int[][] box;
-    static boolean[][] visited;
-    static int[] dn = {-1, 1, 0, 0, 0, 0};
-    static int[] dm = {0, 0, -1, 1, 0, 0};
-    static Queue<Node> queue = new LinkedList<>();
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        box = new int[n][m];
-        visited = new boolean[n][m];
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] box = new int[n][m];
+        Queue<int[]> queue = new ArrayDeque<>();
 
-        boolean isAllDone = true;
+        int emptyCnt = 0;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                String inputs = st.nextToken();
-                int tomato = inputs.length() == 1 ? Integer.parseInt(inputs) : -1;
-                if (tomato != 1) isAllDone = false;
-                if (tomato == 1) {
-                    queue.add(new Node(i, j, 0));
-                    visited[i][j] = true;
+                box[i][j] = Integer.parseInt(st.nextToken());
+                if (box[i][j] == 0) {
+                    emptyCnt++;
+                } else if (box[i][j] == 1) {
+                    queue.offer(new int[]{i, j});
                 }
-                box[i][j] = tomato;
             }
         }
-        br.close();
 
-        if (isAllDone) {
+        if (emptyCnt == 0) {
             System.out.println(0);
             return;
         }
 
-        System.out.println(bfs());
-    }
-
-    private static int bfs() {
-
-        int result = 0;
+        int cnt = 0;
+        int answer = 0;
 
         while (!queue.isEmpty()) {
-            Node current = queue.poll();
+            int[] cur = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
 
-            for (int i = 0; i < 6; i++) {
-                int nn = current.n + dn[i];
-                int nm = current.m + dm[i];
-
-                if (nn < 0 || nm < 0 || nn >= n || nm >= m) continue;
-
-                if (box[nn][nm] == -1) continue;
-
-                if (!visited[nn][nm] && box[nn][nm] == 0) {
-                    visited[nn][nm] = true;
-                    box[nn][nm] = 1;
-                    result = current.count + 1;
-                    queue.offer(new Node(nn, nm, result));
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                    continue;
                 }
+
+                if (box[nx][ny] != 0) {
+                    continue;
+                }
+
+                // 토마토가 익은 일수 자체를 box에 저장
+                box[nx][ny] = box[cur[0]][cur[1]] + 1;
+                queue.offer(new int[]{nx, ny});
+                cnt++;
             }
         }
 
-        // 익지 않은 토마토 확인
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (box[i][j] == 0) {
-                    return -1;
-                }
+                answer = Math.max(answer, box[i][j]);
             }
         }
 
-        return result;
-    }
-
-    private static class Node {
-        int n;
-        int m;
-        int count;
-
-        public Node(int n, int m, int count) {
-            this.n = n;
-            this.m = m;
-            this.count = count;
-        }
+        // box 값이 1부터 시작이니까 (최대값 - 1)이 정답
+        System.out.println(cnt == emptyCnt ? answer - 1 : -1);
     }
 }
